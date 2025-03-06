9acccee65277594e9303ffd8611483e8cc518f8b
p30129tp <talin.patel@student.manchester.ac.uk>
2025-03-06 12:08:35 +0000

    add param struct

--- simulation/physics_c/physics.h
+++ simulation/physics_c/physics.h
@@ -4,10 +4,44 @@ typedef struct {
 
 typedef struct {
 
+	double acc, vel, delta;
+
 } Input;
 
 typedef struct {
+	typedef struct{
+		double m,g,I_z,C_f,C_r;
+	}
+	Inertia;
 
+	typedef struct{
+		double l, b_F, b_R, w_front, l_F, l_R, axle_width;
+	}
+	Kinematic;
+
+	typedef struct{
+		double tire_coefficient, B, C, D, E, radius;
+	}
+	Tire;
+
+	typedef struct{
+		double c_down, c_drag;
+	} Aero;
+
+	typedef struct{
+		typedef struct{
+			double min, max;
+		} Range;
+		
+	Range acc, vel, delta;
+	}
+	InputRanges;
+
+	Inertia inertia;
+	Kinematic kinematic;
+	Tire tire;
+	Aero aero;
+	InputRanges input_ranges;
 } Param;
 
 // Not defining WheelInfo as it is not used
