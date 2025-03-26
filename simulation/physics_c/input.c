6ffe75ba4602aa75240427ef69fe349d61b1ca34
Joshua Bode <joshua.bode@student.manchester.ac.uk>
2025-03-26 01:16:52 +0000

    Removed simulation folder since it is still under development

--- simulation/physics_c/input.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "physics.h"
-
-char* stringifyInput(Input input){
-	static char str[256];
-    sprintf(str, "acc: %f | vel: %f | delta: %f\n", input.acc, input.vel, input.delta);
-    return str;
-}
